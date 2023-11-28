6237a24daf2a29d656af32474d3b58794dd3a540
K1selev <serge2000k@gmail.com>
2023-11-28 07:03:46 +0300

    initial commit

---
+++ ARtivity/ARtivity/TabBar.swift
@@ -0,0 +1,45 @@
+//
+//  TabBar.swift
+//  ARtivity
+//
+//  Created by Сергей Киселев on 28.11.2023.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+
+        modalTransitionStyle = .coverVertical
+        modalPresentationStyle = .fullScreen
+        self.tabBar.backgroundColor = .white
+        let eventVC = ViewController()
+//        let eventVCe = UINavigationController(rootViewController: eventVC)
+
+        let mapVC = ViewController()
+        let addVC = ViewController()
+        let profileVC = ViewController()
+        
+        let navVC = UINavigationController(rootViewController: eventVC)
+//        present(navVC, animated: true)
+
+        eventVC.tabBarItem = UITabBarItem(title: "eventText",
+                                          image: UIImage(systemName: "message"),
+                                          selectedImage: UIImage(systemName: "message.fill"))
+        mapVC.tabBarItem = UITabBarItem(title: "mapText",
+                                        image: UIImage(systemName: "map"),
+                                        selectedImage: UIImage(systemName: "map.fill"))
+        addVC.tabBarItem = UITabBarItem(title: "addText",
+                                        image: UIImage(systemName: "plus.circle"),
+                                        selectedImage: UIImage(systemName: "plus.circle.fill"))
+        profileVC.tabBarItem = UITabBarItem(title: "profileText",
+                                            image: UIImage(systemName: "person"),
+                                            selectedImage: UIImage(systemName: "person.fill"))
+        viewControllers = [navVC, mapVC, addVC, profileVC]
+        selectedIndex = 0
+    }
+}
+
---
+++ ARtivity/ARtivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARtivity
+//
+//  Created by Сергей Киселев on 28.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
